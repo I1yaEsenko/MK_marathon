d1182379ec1a5eefabc8e98295de2784b26dd15d
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-14 20:26:51 +0300

    Add object players

---
+++ main.js
@@ -0,0 +1,19 @@
+const player1 = {
+  name:'Sonya',
+  hp:100,
+  img:'',
+  weapon:[polearm, knife],
+  attack: function(){
+    console.log(player1.name + 'Fight ...' )
+  }
+};
+
+const player2 = {
+  name:'Lu-Kang',
+  hp:100,
+  img:'',
+  weapon:[blade, shield],
+  attack: function(){
+    console.log(player1.name + 'Fight ...' )
+  }
+};
