cab39021a94805a4e912da17505a417045dfc531
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-14 21:28:38 +0300

    add task 1 and task 2

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
 const player1 = {
   name:'Sonya',
   hp:100,
-  img:'',
-  weapon:[polearm, knife],
+  img:'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+  weapon:['polearm', 'knife'],
   attack: function(){
     console.log(player1.name + 'Fight ...' )
   }
 };
 
 const player2 = {
-  name:'Lu-Kang',
+  name:'LiuKang',
   hp:100,
-  img:'',
-  weapon:[blade, shield],
+  img:'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+  weapon:['blade', 'shield'],
   attack: function(){
-    console.log(player1.name + 'Fight ...' )
+    console.log(player2.name + 'Fight ...' )
   }
 };
+
+function createPlayer(player, playerName, hp, heroImg ){
+  const $player = document.createElement('div');
+  $player.classList.add(player);
+ 
+  const $progressbar = document.createElement('div');
+  $progressbar.classList.add('progressbar');
+ 
+  const $character = document.createElement('div');
+  $character.classList.add('character');
+ 
+  const $img = document.createElement('img');
+  $img.src = 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif';
+
+  const $life = document.createElement('div');
+  $life.classList.add('life');
+  $life.style.width = '100%';
+
+  const $name = document.createElement('div');
+  $name.classList.add('name');
+  $name.innerText = playerName.name;
+
+  const $main = document.querySelector('.arenas');
+  $main.appendChild($player);
+
+  $player.appendChild($progressbar);
+  $player.appendChild($character);
+  $progressbar.appendChild($life);
+  $progressbar.appendChild($name);
+  $character.appendChild($img);
+  
+  
+}
+
+createPlayer('player1.name', 'player1.hp', 'player1.img');
+createPlayer('player2.name', 'player2.hp', 'player2.img');
