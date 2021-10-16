05ababe5e0be3fa34b0571004bf6cb8f62e96b22
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-17 00:55:01 +0300

    update task 1-3

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const $main = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+$main.classList.add('arena'+Math.ceil(Math.random() * 5)); //рандомный фон арены
 
 const player1 = {
   player: 1,
   name:'Sonya',
-  hp:"40",
+  hp:"100",
   img:'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
   weapon:['polearm', 'knife'],
   attack: function(name){
@@ -22,11 +24,11 @@ const player2 = {
   }
 };
 
+
 function createElement(tag, className){
     const $tag = document.createElement(tag);
     if (className){
       $tag.classList.add(className);
-
     }
     return $tag;
 }
@@ -38,10 +40,11 @@ function createPlayer(playerObject){
   const $img = createElement('img');
   const $life = createElement('div', 'life');
   const $name = createElement('div', 'name');
-
+  
   $life.style.width = playerObject.hp + '%';
   $name.innerText = playerObject.name;
   $img.src = playerObject.img;
+  
 
   $player.appendChild($progressbar);
   $player.appendChild($character);
@@ -50,6 +53,56 @@ function createPlayer(playerObject){
   $character.appendChild($img);
   return $player;
 }
+
+
+function randomizerHp() {  // рандомное число для уменьшения hp
+  let hp = Math.ceil(Math.random() * 20);
+  return hp;
+}
+
+function changeHp (player){ // Уменьшение здоровья
+  const $playerLife = document.querySelector('.player'+ player.player +' .life');
+  player.hp -= randomizerHp();
+  $playerLife.style.width = player.hp + '%';
+  if (player.hp < 0){
+    player.hp = 0 ;
+    $playerLife.style.width = 0 + '%';
+    $randomButton.disabled = true
+  }
+} 
+
+function playerWin(name){ // Победа
+  const $winTitle = createElement('div', 'winTitle');
+  $winTitle.innerText = name + ' WIN';
+  return $winTitle;
+}
+
+function draw() { // Ничья
+  const $drawTitle = createElement('div', 'drawTitle');
+  $drawTitle.innerText = 'Draw!';
+  return $drawTitle;
+}
+
+function compareHp (numberOne, numberTwo) { //Cравнение здоровья
+  const $hpOne = document.querySelector('.player'+ numberOne.player +' .life');
+  const $hpTwo = document.querySelector('.player'+ numberTwo.player +' .life');
+
+  if (numberOne.hp > 0 && numberTwo.hp <= 0) {
+      $main.appendChild(playerWin(numberOne.name));
+  } else if (numberOne.hp <= 0 && numberTwo.hp > 0) {
+      $main.appendChild(playerWin(numberTwo.name));
+  } else if (numberOne.hp <= 0 && numberTwo.hp <= 0) {
+      $main.appendChild(draw());
+  }
+}
+
+$randomButton.addEventListener('click', function() {
+  console.log('1st ' + player1.hp);
+  console.log('2nd ' + player2.hp);
+  changeHp(player1);
+  changeHp(player2);
+  compareHp (player1, player2);
+})
 
 
 $main.appendChild(createPlayer(player1));
